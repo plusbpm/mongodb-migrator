6bc0b39b33b871089b7afe482f71001b42b8efbb
plusbpm <plus-bpm@yandex.ru>
2016-06-02 00:08:24 +0300

    начало

---
+++ resources/template.js
@@ -0,0 +1,10 @@
+module.exports = {
+	id: '${id}',
+	afterDeps: [${afterDeps}],
+	up: function (db, cb) {
+		cb();
+	},
+	down: function (db, cb) {
+		cb();
+	}
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+var mm = require("./");
+
+var mmm = new mm({
+	host: 'asdasd',
+});
+
+mmm.create("просто проверка", ['sdasd1','sasdasd2']);
+
